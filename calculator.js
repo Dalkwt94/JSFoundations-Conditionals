beb255b0faa10d13cb063af57a59cd094989c3d2
Dee Macbook <deemacbook@Dees-MacBook-Pro.local>
2020-06-15 20:28:47 +0300

    solved conditions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
 // Define the numbers and the operation here
+const num1 = 5;
+const num2 = 3;
+let op = "-";
+
+//num1 + num2;
 
 // Write if-statements here
+
+if (op === "+") {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`);
+} else if (op === "-") {
+  console.log(`${num1} - ${num2} = ${num1 - num2}`);
+} else if (op === "*") {
+  console.log(`${num1} * ${num2} = ${num1 * num2}`);
+} else {
+  console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
